c788ee129b7fcb98cbfc6b4d36f2a34273a13a6d
Tim Johnson <pavementii@gmail.com>
2019-04-12 09:16:09 -0400

    [test] add test for isSquare()

--- magic-square.test.js
+++ magic-square.test.js
@@ -2,9 +2,26 @@ const magicSquare = require('./magic-square')
 
 describe('isMagicSquare()', () => {
   test(' [ [2,7,6], [9,5,1], [4, 3, 8] ] should return true', () => {
-    
-    var magicSquare = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    var testSquare = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    var dog
 
     expect(magicSquare.isMagicSquare(magicSquare)).toEqual(true)
+  })
+})
+
+describe('isSquare()', () => {
+  test('return false if square is not an array', () => {
+    var testSquare = ''
+    expect(magicSquare.isSquare(testSquare)).toEqual(false)
+  })
+
+  test('return false if square is not an n x n array', () => {
+    var testSquare = [[2, 7, 6], [9, 5, 1]]
+    expect(magicSquare.isSquare(testSquare)).toEqual(false)
+  })
+
+  test('return true if square is an n x n array', () => {
+    var testSquare = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    expect(magicSquare.isSquare(testSquare)).toEqual(false)
   })
 })
