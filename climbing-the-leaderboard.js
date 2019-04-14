11bb535ac25fa3151724343cb1839f1414ba2bcc
Tim <pavementii@gmail.com>
2019-04-13 20:35:10 -0400

    [fc] refactor to complete in O(M + change)

--- climbing-the-leaderboard.js
+++ climbing-the-leaderboard.js
@@ -9,52 +9,37 @@ alice: an array of integers that represent Alice's scores?
 
 // Complete the climbingLeaderboard function below.
 function climbingLeaderboard (scores, alice) {
-
-  
   let scoreboard = {}
+  let score = ''
+  let rank = 1
+  let aIndex = alice.length - 1
   let aliceRanking = []
-  let currentScore = ''
-
-  // create scoreboard
-  scores.forEach(score => {
-    addScore(score, scoreboard)
-  })
-
-  for (var i = 0; i < alice.length; i++) {
-    currentScore = alice[i]
-    // remove alice's oldPosition (if any) from scoreboard
-    if (i > 0) {
-      removeScore(alice[i - 1], scoreboard)
+  
+  // test Alice's best score
+  if (alice[aIndex] >= scores[0]) {
+    aliceRanking.push(rank)
+    aIndex--
+  }
+  
+  // filter unique while simultaneously testing alice's score
+  for (let i = 0; i < scores.length; i++) {
+    score = scores[i]
+    if (!scoreboard[score]) {
+      scoreboard[score] = rank
+    
+      while (alice[aIndex] >= score) {
+        aliceRanking.unshift(rank)
+        aIndex--
+      }
+      rank++
     }
-    // add newPosition
-    addScore(currentScore, scoreboard)
-   
-    // create array of scoreboard object, sort it
-    scores = Object.keys(scoreboard).reverse()
+  }
 
-    // return indexOf newPosition'
-    aliceRanking.push(scores.indexOf(currentScore.toString()) + 1)
+  // finish looping through Alice's worst scores
+  while(aIndex >= 0){
+    aliceRanking.unshift(rank)
+    aIndex--
   }
 
   return aliceRanking
 }
-
-function addScore (score, scoreboard) {
-  if (!scoreboard[score]) {
-    scoreboard[score] = 1
-  } else {
-    scoreboard[score]++
-  }
-}
-
-function removeScore (score, scoreboard) {
-  scoreboard[score]--
-  if (scoreboard[score] === 0) {
-    delete scoreboard[score]
-  }
-}
-
-const scores = [100, 100, 50, 40, 40, 20, 10]
-const alice = [5, 25, 50, 120]
-
-climbingLeaderboard(scores, alice)
