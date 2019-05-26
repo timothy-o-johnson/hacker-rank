9a6516cac45ecf2d98dcccc0d1321ba7b2a0ac92
Tim <pavementii@gmail.com>
2019-05-25 23:32:58 -0400

    [fc]  remove regex replace with obj

--- grid-search.js
+++ grid-search.js
@@ -12,36 +12,55 @@ P: the pattern to search for, an array of strings */
 function gridSearch (grid, pattern) {
   let i = 0
   let j = 0
-  let str
-  let regexp
+  let gridRow
+  let patternRow
   const numOfPatternRows = pattern.length
-  let matchArr = []
   let row
   let col
   let matches
   let found = false
+  let matchObj = {}
 
-  for (i = 0; i < numOfPatternRows; i++) {
-    regexp = RegExp(pattern[i], 'g')
+  // loop through each row
+  for (j = 0; j < grid.length; j++) {
+    gridRow = grid[j]
+    for (i = 0; i < numOfPatternRows; i++) {
+      patternRow = pattern[i]
 
-    for (j = 0; j < grid.length; j++) {
-      str = grid[j]
-
-      while ((matches = regexp.exec(str)) !== null) {
-        row = i
-        col = regexp.lastIndex
-
-        matchArr.push([row, col])
-        console.log(`Found ${regexp} at row ${row} at ${col}.`)
+      // loop through each char in row
+      for (var k = 0; k < gridRow.length; k++) {
+        for (var l = 0; l < patternRow.length; l++) {
+          // compare grid to pattern
+          if (gridRow[k + l] !== patternRow[l]) {
+            break
+          } else if (gridRow[k + l] === patternRow[l] && l === patternRow.length - 1) {
+            row = j
+            col = k + l - (patternRow.length - 1)
+            // if matches entire row then save to matchArr
+            matchObj[row] = col
+            console.log(`Found patternRow ${i}: ${patternRow} in Grid at row ${row} at ${col}.`)
+          }
+        }
       }
     }
   }
 
-  console.log('matchArr', matchArr)
-  let matchCount = 0
+  console.log('matchObj', JSON.stringify(matchObj))
 
-  for (i = 0; i < matchArr.length - 1; i++) {
-    if (matchArr[i][1] === matchArr[i + 1][1]) {
+  let matchCount = 0
+  const keys = Object.keys(matchObj)
+
+  for (i = 0; i < keys.length; i++) {
+    var first = keys[i]
+    var second = (parseInt(first) + 1).toString()
+
+    // has to follow the row AND match the column
+    if (matchObj[first] !== null && matchObj[second] !== null && matchObj[first] === matchObj[second]) {
+     // console.log('indices', first, second)
+
+     // console.log('matchObj', JSON.stringify(matchObj[i]), JSON.stringify(matchObj[i + 1]))
+     // console.log('matchObj', matchObj[first], matchObj[second])
+
       matchCount++
       found = matchCount === numOfPatternRows - 1
       if (found) {
@@ -77,21 +96,21 @@ var test_1 = {
 
 const test_2 = {
   G: [
-    '400453592126560',
-    '114213133098692',
-    '474386082879648',
-    '522356951189169',
-    '887109450487496',
-    '252802633388782',
-    '502771484966748',
-    '075975207693780',
-    '511799789562806',
-    '404007454272504',
-    '549043809916080',
-    '962410809534811',
-    '445893523733475',
-    '768705303214174',
-    '650629270887160'
+    '400453592126560', // 0
+    '114213133098692', // 1
+    '474386082879648', // 2
+    '522356951189169', // 3
+    '887109450487496', // 4
+    '252802633388782', // 5
+    '502771484966748', // 6
+    '075975207693780', // 7
+    '511799789562806', // 8
+    '404007454272504', // 9
+    '549043809916080', // 10
+    '962410809534811', // 11
+    '445893523733475', // 12
+    '768705303214174', // 13
+    '650629270887160' // 14
   ],
 
   P: ['99', '99']
@@ -116,16 +135,13 @@ const test_5 = {
 }
 
 const test_6 = {
-  G: [
-    '111111111111111', 
-    '111111111111111', 
-    '111111011111111', 
-    '111111111111111', 
-    '111111111111111'],
+  G: ['111111111111111', '111111111111111', '111111011111111', '111111111111111', '111111111111111'],
 
-  P: [
-    '11111', 
-    '11111', 
-    '11110']
+  P: ['11111', '11111', '11110']
 }
 gridSearch(test_6.G, test_6.P)
+gridSearch(test_5.G, test_5.P)
+gridSearch(test_4.G, test_4.P)
+gridSearch(test_3.G, test_3.P)
+gridSearch(test_2.G, test_2.P)
+gridSearch(test_1.G, test_1.P)
