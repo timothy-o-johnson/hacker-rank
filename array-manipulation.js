8bc1cfaecf3b46feed1cfb0ef7621486596b92b1
pavementii@gmail.com <pavementii@gmail.com>
2020-05-30 10:06:55 -0500

    refactor

--- array-manipulation.js
+++ array-manipulation.js
@@ -1,41 +1,42 @@
-function arrayManipulation(lengthOfArray, queries){
+function arrayManipulation(lengthOfArray, queries) {
     var solutionObj = {
         max: 0
     }
 
+    var startingIndex, endingIndex, value
     var i, current, max
-    
-    queries.forEach(function (query){
-        var startingIndex = query[0]
-        var endingIndex = query[1]
-        var value = query[2]
 
-        console.log('startingIndex | endingIndex | value ::', startingIndex + ' | ' +  endingIndex + ' | ' + value );
+    queries.forEach(function (query) {
+        startingIndex = query[0]
+        endingIndex = query[1]
+        value = query[2]
+
+        console.log('startingIndex | endingIndex | value ::', startingIndex + ' | ' + endingIndex + ' | ' + value);
 
         // populate object according to the rules of the query
-        for(i = startingIndex; i < endingIndex; i++){
-            if(!solutionObj[i]){
+        for (i = startingIndex; i < endingIndex; i++) {
+            if (!solutionObj[i]) {
                 solutionObj[i] = value
-            } else{
-                solutionObj[i] = current + value               
+            } else {
+                solutionObj[i] = current + value
             }
 
             current = solutionObj[i]
-            max =  solutionObj['max']
+            max = solutionObj['max']
 
-            solutionObj['max'] =  current > max ? current : max
+            solutionObj['max'] = current > max ? current : max
         }
-        
+
     })
 
     // console.log(`solutionObj['max']: `, solutionObj['max']);
     // console.log(`solutionObj`, solutionObj);
     // console.log('solution: ', solution);
-    
+
     return solutionObj['max']
 }
 
 
 module.exports = {
     arrayManipulation
-  }
+}
