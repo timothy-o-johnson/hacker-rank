629efe779a285057c9a757b8158565ca793b5b14
pavementii@gmail.com <pavementii@gmail.com>
2020-05-30 07:40:34 -0500

    return solution when given a query of length 2

--- array-manipulation.js
+++ array-manipulation.js
@@ -1,5 +1,27 @@
-function arrayManipulation(lengthOfArray, query){
-    return 3
+function arrayManipulation(lengthOfArray, queries){
+    var solution
+    var array = new Array (lengthOfArray).fill(0)
+    var i
+    
+    queries.forEach(function (query){
+        var startingIndex = query[0] - 1
+        var endingIndex = query[1] - 1
+        var value = query[2]
+
+        console.log('startingIndex | endingIndex | value ::', startingIndex + ' | ' +  endingIndex + ' | ' + value );
+
+        // populate the array according to the rules of the query
+        for(i = startingIndex; i< endingIndex; i++){
+            array[i] += value
+        }
+        
+    })
+
+    solution = Math.max(...array)
+
+    console.log(solution);
+    
+    return solution
 }
 
 
